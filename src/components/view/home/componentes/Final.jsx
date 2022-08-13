50ce81ef4f7e05a9e8b2cafad5833d2873dc1ec6
jeferson <erwin.alexander@gmail.com>
2022-08-13 10:25:51 -0400

    final v1

--- src/components/view/home/componentes/Final.jsx
+++ src/components/view/home/componentes/Final.jsx
@@ -18,59 +18,59 @@ function Final({
 
       <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
 
-          <div className="col">
-            <div
-              className="shadow  card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style={{
-                backgroundImage: `url(${fondo1})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center center',
-                backgroundSize: 'cover'
-              }}
-            >
-              <div className="d-flex flex-column h-100 p-5 text-white text-shadow-1">
-                <h2 className="my-auto py-5 display-6 lh-1 fw-bold">{titulo1}</h2>
-              </div>
+        <div className="col">
+          <div
+            className="shadow  card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+            style={{
+              backgroundImage: `url(${fondo1})`,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center center",
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="d-flex flex-column h-100 p-5 text-white text-shadow-1">
+              <h2 className="my-auto py-5 display-6 lh-1 fw-bold">{titulo1}</h2>
             </div>
           </div>
+        </div>
 
-
-          <div className="col">
-            <div
-              className="shadow card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style={{
-                background: `url(${fondo2})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center center',
-                backgroundSize: 'cover'
-              }}
-            >
-              <div className="d-flex flex-column h-100 p-5 text-white text-shadow-1">
-                <h2 className="my-auto py-5 display-6 lh-1 fw-bold">{titulo2}</h2>
-              </div>
+        <div className="col">
+          <div
+            className="shadow card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+            style={{
+              background: `url(${fondo2})`,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center center",
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="d-flex flex-column h-100 p-5 text-white text-shadow-1">
+              <h2 className="my-auto py-5 display-6 lh-1 fw-bold">{titulo2}</h2>
             </div>
           </div>
+        </div>
+
+        <div className="col">
+          <div
+            className="shadow card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+            style={{
+              background: `url(${fondo3})`,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center center",
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="d-flex flex-column h-100 p-5  text-wrap">
+              <h2 className="my-auto py-5 display-6 lh-1 fw-bold ">
+                {titulo3}
+              </h2>
+            </div>
+          </div>
+        </div>
         
-          <div className="col">
-            <div
-              className="shadow card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style={{
-                background: `url(${fondo3})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center center',
-                backgroundSize: 'cover'
-              }}
-            >
-              <div className="d-flex flex-column h-100 p-5  text-wrap">
-                <h2 className="my-auto py-5 display-6 lh-1 fw-bold ">
-                  {titulo3}
-                </h2>
-              </div>
-            </div>
-          </div>
       </div>
     </div>
   );
-};
+}
 
 export default Final;
