a027472c7720ca08a2643568e6a2dca153596f50
unknown <peedroaleex@gmail.com>
2022-08-12 21:26:46 -0400

    modificacin version1 home

--- src/components/view/home/componentes/Final.jsx
+++ src/components/view/home/componentes/Final.jsx
@@ -68,7 +68,7 @@ function Final({ fondo1, fondo2, fondo3, titulo1, titulo2, titulo3, link1, link2
               <ul class="d-flex list-unstyled mt-auto">
                 <li class="me-auto">
                   <img
-                    src="https://github.com/twbs.png"
+                    src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/elle-plantas-flores-verano-1625816917.jpg?resize=640:*"
                     alt="Bootstrap"
                     width="32"
                     height="32"
@@ -105,7 +105,7 @@ function Final({ fondo1, fondo2, fondo3, titulo1, titulo2, titulo3, link1, link2
               <ul class="d-flex list-unstyled mt-auto">
                 <li class="me-auto">
                   <img
-                    src="https://github.com/twbs.png"
+                    src="https://www.hogarmania.com/archivos/202202/calendario-fruta-verduras-temporada-668x400x80xX-1.jpg"
                     alt="Bootstrap"
                     width="32"
                     height="32"
