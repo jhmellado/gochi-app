b91c4ea0c84a8e21e72a9bdeb7b876e007b298eb
jeferson <erwin.alexander@gmail.com>
2022-08-13 15:09:33 -0400

    sacar lista api vista nosotros

--- src/components/view/Nosotros.jsx
+++ src/components/view/Nosotros.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import { useEffect } from "react";
 import "../../stylesheets/Nosotros.css";
 import Nosotro from "./nosotros/Nosotro";
@@ -7,36 +6,13 @@ import Nosotro from "./nosotros/Nosotro";
 const Nosotros = () => {
   useEffect(() => {
     document.title = "Gochi - Cursos";
-    obtenerDatos();
   }, []);
-
-  const [equipo, setEquipo] = useState([]);
-
-  const obtenerDatos = async () => {
-    const data = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await data.json();
-    setEquipo(users);
-  };
 
   return (
     <div className="container contenedor-todo pt-1">
       <Nosotro titulo="hola" descripcion="loremmmmmmm" />
       <Nosotro></Nosotro>
       <Nosotro></Nosotro>
-
-      {/*api */}
-      <div className="container pt-3 ">
-        <div className="text-center ">
-          <ul className="list-inline">
-            {equipo.map((item) => (
-              <li key={item.id}>
-                {item.name} - {item.email} - {item.address.street}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <hr class="border border-4"></hr>
-      </div>
     </div>
   );
 };
