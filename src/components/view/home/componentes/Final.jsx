4235f7f4a60a2f9a19259db69c22fc6c61679639
jeferson <erwin.alexander@gmail.com>
2022-08-13 04:21:31 -0400

    final ok

--- src/components/view/home/componentes/Final.jsx
+++ src/components/view/home/componentes/Final.jsx
@@ -17,129 +17,60 @@ function Final({
       <h2 className="pb-2 border-bottom">Conoce sobre..</h2>
 
       <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-        <a
-          className="tarjetita"
-          href={link1}
-          style={{ textDecoration: "none" }}
-        >
-          <div className="col tarjetita">
-            <div
-              className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style={{
-                background: `url(${fondo1})`,
-              }}
-            >
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{titulo1}</h2>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                    <img
-                      src="https://www.hogarmania.com/archivos/201910/remedios-naturales-con-plantas-medicinales-848x477x80xX.jpg"
-                      alt="Bootstrap"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#geo-fill"></use>
-                    </svg>
-                    <small>Ir</small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <BsFillArrowRightSquareFill />
-                    <small>{/*un texto*/}</small>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </a>
 
-        <a
-          className="tarjetita"
-          href={link2}
-          style={{ textDecoration: "none" }}
-        >
-          <div className="col tarjetita">
-            <div
-              className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style={{
-                background: `url(${fondo2})`,
-              }}
-            >
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{titulo2}</h2>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                    <img
-                      src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/elle-plantas-flores-verano-1625816917.jpg?resize=640:*"
-                      alt="Bootstrap"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#geo-fill"></use>
-                    </svg>
-                    <small>Ir</small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <BsFillArrowRightSquareFill />
-                    <small>{/*un texto*/}</small>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </a>
-        <a
-          className="tarjetita"
-          href={link3}
-          style={{ textDecoration: "none" }}
-        >
           <div className="col">
             <div
-              className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              className="shadow  card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
               style={{
-                background: `url(${fondo3})`,
+                backgroundImage: `url(${fondo1})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center center',
+                backgroundSize: 'cover'
               }}
             >
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-                <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold ">
-                  {titulo3}
-                </h2>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                    <img
-                      src="https://www.hogarmania.com/archivos/202202/calendario-fruta-verduras-temporada-668x400x80xX-1.jpg"
-                      alt="Bootstrap"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#geo-fill"></use>
-                    </svg>
-                    <small>Ir</small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <BsFillArrowRightSquareFill />
-                    <small>{/*un texto*/}</small>
-                  </li>
-                </ul>
+              <div className="d-flex flex-column h-100 p-5 text-white text-shadow-1">
+                <h2 className="my-auto py-5 display-6 lh-1 fw-bold">{titulo1}</h2>
               </div>
             </div>
           </div>
-        </a>
+
+
+          <div className="col">
+            <div
+              className="shadow card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              style={{
+                background: `url(${fondo2})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center center',
+                backgroundSize: 'cover'
+              }}
+            >
+              <div className="d-flex flex-column h-100 p-5 text-white text-shadow-1">
+                <h2 className="my-auto py-5 display-6 lh-1 fw-bold">{titulo2}</h2>
+              </div>
+            </div>
+          </div>
+        
+          <div className="col">
+            <div
+              className="shadow card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              style={{
+                background: `url(${fondo3})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center center',
+                backgroundSize: 'cover'
+              }}
+            >
+              <div className="d-flex flex-column h-100 p-5  text-wrap">
+                <h2 className="my-auto py-5 display-6 lh-1 fw-bold ">
+                  {titulo3}
+                </h2>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   );
-}
+};
 
 export default Final;
