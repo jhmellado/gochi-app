ed6ed0adaf58376f087ee1520b1f6bda5b538987
jeferson <erwin.alexander@gmail.com>
2022-08-29 17:41:05 -0400

    bug imagen previa arreglado

--- src/components/view/configuracion/componentes/ImagenPerfil.jsx
+++ src/components/view/configuracion/componentes/ImagenPerfil.jsx
@@ -1,81 +1,107 @@
 import React, { useState } from "react";
-import { auth, storage,firebase} from "../../../../firebase";
+import { auth, storage, firebase } from "../../../../firebase";
 
 const ImagenPerfil = (props) => {
   const [error, setError] = useState(null);
   const [imagenInput, setImagenInput] = useState("");
+  
 
-  const procesarApellido = (event) => {
+  const actualizarFotoPerfil = (event) => {
     event.preventDefault();
+    console.log(event);
     if (!imagenInput.trim()) {
-      setError("Ingrese su apellido");
+      setError("Ingrese una imagen por favor");
       return;
     }
+    console.log("subiendo imagen");
     setError(null);
   };
 
   const imagenPrevia = (imagen) => {
-    var storageRef = storage.ref();
-    var listRef = storageRef.child("vista_previa/"+auth.currentUser.uid);
-    listRef.listAll()
-    .then((res) => {
-      res.items.forEach((itemRef) => {
-        var desertRef = storageRef.child("vista_previa/"+auth.currentUser.uid+'/'+itemRef.name);
-        desertRef.delete().then(() => {
-          console.log('File deleted successfully');
-        }).catch((error) => {
+    if (imagen.files.length !== 0) {
+      var storageRef = storage.ref();
+      var listRef = storageRef.child("vista_previa/" + auth.currentUser.uid);
+      listRef
+        .listAll()
+        .then((res) => {
+          if (res.items.length > 0) {
+            res.items.forEach((itemRef) => {
+              var desertRef = storageRef.child(
+                "vista_previa/" + auth.currentUser.uid + "/" + itemRef.name
+              );
+              desertRef
+                .delete()
+                .then(() => {
+                  console.log("File deleted successfully");
+                })
+                .catch((error) => {
+                  console.log(error);
+                });
+            });
+          } else {
+            console.log("No hay elementos");
+          }
+        })
+        .then(() => {
+          var imagenRef = storageRef
+            .child(
+              "vista_previa/" + auth.currentUser.uid + "/" + imagen.files.name
+            )
+            .put(imagen.files[0]);
+          imagenRef.on(
+            "state_changed",
+            (snapshot) => {
+              var progress =
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log("Upload is " + progress + "% done");
+              switch (snapshot.state) {
+                default:
+                  console.log("Algo anda mal");
+                  break;
+                case firebase.storage.TaskState.PAUSED:
+                  console.log("Upload is paused");
+                  break;
+                case firebase.storage.TaskState.RUNNING:
+                  console.log("Upload is running");
+                  break;
+              }
+            },
+            (error) => {
+              switch (error.code) {
+                default:
+                  console.log("Algo anda mal");
+                  break;
+                case "storage/unauthorized":
+                  console.log(error.message);
+                  // ('User doesn't have permission to access the object')
+                  break;
+                case "storage/canceled":
+                  console.log(error.message);
+                  // User canceled the upload
+                  break;
+
+                case "storage/unknown":
+                  console.log(error.message);
+                  // Unknown error occurred, inspect error.serverResponse
+                  break;
+              }
+            },
+            () => {
+              imagenRef.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                console.log("File available at", downloadURL);
+                setImagenInput(downloadURL);
+              });
+            }
+          );
+        })
+        .catch((error) => {
           console.log(error);
         });
-      });
-    }).catch((error) => {
-        console.log(error);
-    });
-    var imagenRef = storageRef
-      .child("vista_previa/"+auth.currentUser.uid+'/'+imagen.name)
-      .put(imagen);
-    imagenRef.on('state_changed', (snapshot) => {
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log("Upload is " + progress + "% done");
-      switch (snapshot.state) {
-        default:
-          console.log('Algo anda mal')
-          break;
-        case firebase.storage.TaskState.PAUSED:
-          console.log("Upload is paused");
-          break;
-        case firebase.storage.TaskState.RUNNING:
-          console.log("Upload is running");
-          break;
-      }
-    },
-    (error) => {
-      switch (error.code) {
-        default:
-          console.log('Algo anda mal')
-          break;
-        case 'storage/unauthorized':
-          console.log(error.message)
-          // ('User doesn't have permission to access the object')
-          break;
-        case 'storage/canceled':
-          console.log(error.message)
-          // User canceled the upload
-          break;
-  
-        // ...
-  
-        case 'storage/unknown':
-          console.log(error.message)
-          // Unknown error occurred, inspect error.serverResponse
-          break;
-      }
-    },
-    () => {
-      imagenRef.snapshot.ref.getDownloadURL().then((downloadURL) => {
-        console.log('File available at', downloadURL);
-        setImagenInput(downloadURL);
-      });
-    });
+      /* */
+    } else {
+      console.log("no ha seleccionado imagen");
+      setImagenInput("");
+    }
   };
 
   return (
@@ -91,7 +117,7 @@ const ImagenPerfil = (props) => {
       <hr />
       <div className="container py-5">
         <div className="d-flex row justify-content-center">
-          <div className="col-12 col-md-6 px-0">
+          <div className="col-12 col-md-8 px-0">
             <div className="container">
               {error && <div className="alert alert-danger">{error}</div>}
               <p>
@@ -127,7 +153,7 @@ const ImagenPerfil = (props) => {
                 </div>
               </div>
             </div>
-            <form onSubmit={procesarApellido}>
+            <form onSubmit={actualizarFotoPerfil}>
               <div className="container mb-4">
                 <div className="d-flex row align-items-end">
                   <div className="col">
@@ -142,7 +168,7 @@ const ImagenPerfil = (props) => {
                       className="form-control"
                       id="file_form"
                       accept=".jpg,.jpeg,.png"
-                      onChange={(event) => imagenPrevia(event.target.files[0])}
+                      onChange={(event) => imagenPrevia(event.target)}
                     />
                   </div>
                 </div>
