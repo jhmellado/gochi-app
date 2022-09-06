d1669936d18e8af6089c6c554941fbda804ff350
jhmellado <jeferson.mellado@cftaraucania.cl>
2022-09-06 13:04:26 -0400

    categoriacultivo ok

--- src/components/view/CategoriaCultivos.jsx
+++ src/components/view/CategoriaCultivos.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
 import { withRouter } from "react-router";
-import { auth } from "../../firebase";
-import Cargando from "../component/Cargando";
 import BannerPublicidad from "./componentesCultivos/BannerPublicidad";
 import EncabezadoCultivo from "./componentesCultivos/EncabezadoCultivo";
 import ComponenteCategoriasCultivo from "./componentesCultivos/ComponenteCategoriasCultivo";
@@ -9,12 +7,6 @@ import { LazyLoadComponent } from "react-lazy-load-image-component";
 
 const CategoriaCultivos = (props) => {
   useEffect(() => {
-    /*if (auth.currentUser) {
-      console.log("ok");
-    } else {
-      //console.log(auth.currentUser);
-      props.history.push("/login");
-    }*/
     document.title = "Gochi - Categorías";
   }, [props.history]);
 
